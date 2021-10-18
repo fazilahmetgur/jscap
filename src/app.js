c0862608e269bdd5e23026851103e9d6eec0a36e
Fazıl Ahmet Gür <fazilahmetgur@gmail.com>
2021-10-19 00:06:13 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,13 @@
+console.log("Merhaba Kodlama İo")
+
+let dolarDun= 9.20
+let dolarBugun=9.33
+
+console.log(dolarDun)
+console.log(dolarBugun)
+let konutKredileri=["çalışan konut kresisi","Emekli Konut Kredisi","Öğrenci Konut Kredisi"]
+console.log(konutKredileri)
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
